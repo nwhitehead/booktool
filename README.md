1002dc2fb1ae81a0ff1f5b352d20ce5124ef4db1
Nathan Whitehead <nwhitehe@gmail.com>
2024-08-17 10:47:33 -0600

    Trying to get project going.

---
+++ README.md
@@ -0,0 +1,5 @@
+# BookTool
+
+This is a standalone webapp for creating books.
+
+Idea is you write your book in Markdown, the app converts it to PDF.
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+function addHeaders(server) {
+    server.middlewares.use((_req, res, next) => {
+        if (_req.url === '/' || _req.url === '/index.html') {
+        } else {
+            res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+            res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+        }
+        next();
+    });
+}
+
+export default defineConfig({
+    build: {
+        target: [ 'es2020', 'chrome67', 'firefox68' ]
+    },
+    output: {
+        inlineDynamicImports: true
+    },
+    plugins: [
+        vue(),
+        {
+            name: "configure-response-headers",
+            configureServer: addHeaders,
+            configurePreviewServer: addHeaders,
+        },
+    ],
+})
