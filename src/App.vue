2150795449775199d1486057250cf830fc92195f
Nathan Whitehead <nwhitehe@gmail.com>
2024-08-18 08:06:59 -0600

    Use theme.

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
                 v-model="localModelValue"
                 :indent-with-tab="true"
                 :tab-size="4"
-                :extensions="[markdownLanguage, oneDark]"
+                :extensions="[markdownLanguage, consoleLightExtension]"
                 :disabled="false"
                 @update:modelValue="newValue => { localModelValue = newValue; $emit('update:modelValue', newValue); }"
                 @ready="handleReady"
@@ -25,10 +25,39 @@
 
 import { ref } from 'vue';
 import { Codemirror } from 'vue-codemirror';
+import { EditorView } from '@codemirror/view';
 import { oneDark } from '@codemirror/theme-one-dark';
 import { markdownLanguage } from '@codemirror/lang-markdown';
+import { consoleLightExtension } from './codemirrorLightTheme.js';
 
-let localModelValue = ref('put stuff here');
+const Theme = EditorView.theme({
+    "&": {
+        fontSize: "10.5pt",
+        border: "1px solid #c0c0c0"
+    },
+    ".cm-content": {
+        fontFamily: "Menlo, Monaco, Lucida Console, monospace",
+        minHeight: "200px"
+    },
+    ".cm-gutters": {
+        minHeight: "200px"
+    },
+    ".cm-scroller": {
+        overflow: "auto",
+        maxHeight: "600px"
+    }
+});
+
+let localModelValue = ref(`
+# Main Title
+
+Some text is here. You can also put [links](http://www.example.com/) to URLs and things.
+
+## Section title
+
+Hopefully this text is looking legible and easy to edit.
+
+`);
 
 function handleReady(event) {
     console.log('CodeMirror is ready.');
