6815ef17156ca9366cacf063db0d9638ce0fe448
Nathan Whitehead <nwhitehe@gmail.com>
2024-08-17 19:09:33 -0600

    Working modelValue in codemirror component.

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,29 @@
 <template>
     <section class="section">
         <h1>The App</h1>
-        <Codemirror></Codemirror>
+        <div>
+            <Codemirror
+                v-model="localModelValue"
+                :indent-with-tab="true"
+                :tab-size="4"
+                :extensions="[]"
+                :disabled="false"
+                @update:modelValue="newValue => { localModelValue = newValue; $emit('update:modelValue', newValue); }"
+                @ready="handleReady"
+            />
+        </div>
     </section>
 </template>
 
 <script setup>
+
+import { ref } from 'vue';
 import { Codemirror } from 'vue-codemirror';
+
+let localModelValue = ref('put stuff here');
+
+function handleReady(event) {
+    console.log('CodeMirror is ready.');
+}
+
 </script>
