979df9ab79f1e2304228249290600a18e3dc1e87
Nathan Whitehead <nwhitehe@gmail.com>
2024-09-03 08:59:58 -0600

    Split out test2 into vue file.

--- app/test2.js
+++ app/test2.js
@@ -1,36 +1,9 @@
-const { createApp, ref, onMounted } = Vue;
+import { createApp } from 'vue';
+import Test2 from './test2.vue';
 
 import './test2style.css';
 
-const app = createApp({
-  setup() {
-    const pages = ref([]);
-
-    // Generate content for pages
-    function generateContent() {
-      console.log('generateContent');
-      // Simulated content for demonstration
-      const content = [
-        "<h1>Page 1</h1><p>This is content for page 1.</p>",
-        "<h1>Page 2</h1><p>This is content for page 2.</p>",
-        "<h1>Page 3</h1><p>This is content for page 3.</p>"
-      ];
-      pages.value = content;
-    }
-    generateContent();
-    onMounted(() => {
-      // console.log('mounted');
-      // let newstyle = document.createElement('style');
-      // newstyle.innerHTML = rawStyle;
-      // document.getElementsByTagName('head')[0].appendChild(newstyle);
-      window.PagedPolyfill.preview();
-    });
-
-    return {
-      pages,
-    };
-  }
-});
+const app = createApp(Test2);
 
 console.log('Going to mount #app');
 app.mount('#app');
---
+++ app/test2.vue
@@ -0,0 +1,36 @@
+<template>
+    <button @click="onClick">Change</button>
+    <div class="myPage" v-for="(pageContent, index) in pages" :key="index">
+        <div v-html="pageContent"></div>
+    </div>
+</template>
+
+<script setup>
+
+import { ref, onMounted } from 'vue';
+
+const pages = ref([]);
+
+// Generate content for pages
+function generateContent() {
+    console.log('generateContent');
+    // Simulated content for demonstration
+    const content = [
+        "<h1>Page 1</h1><p>This is content for page 1.</p>",
+        "<h1>Page 2</h1><p>This is content for page 2.</p>",
+        "<h1>Page 3</h1><p>This is content for page 3.</p>"
+    ];
+    pages.value = content;
+}
+
+generateContent();
+
+onMounted(() => {
+    // console.log('mounted');
+    // let newstyle = document.createElement('style');
+    // newstyle.innerHTML = rawStyle;
+    // document.getElementsByTagName('head')[0].appendChild(newstyle);
+    window.PagedPolyfill.preview();
+});
+
+</script>
