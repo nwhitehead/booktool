0bb02d8dfdd93ffd54cab76529d067b460f457a3
Nathan Whitehead <nwhitehe@gmail.com>
2024-09-05 12:34:44 -0600

    Try to use hypermd.
    It only has require syntax, not esm. Yuck.

--- app/src/App.vue
+++ app/src/App.vue
@@ -17,28 +17,10 @@
 </style>
 
 <template>
-    <p class="blah">Test</p>
-    <div ref="editor"></div>
     <section class="section">
-        <h1>The App</h1>
-        <button @click="() => outputChoice = 'html'">HTML</button>
-        <button @click="() => outputChoice = 'paged'">Paged</button>
-        <button @click="() => outputChoice = 'frontmatter'">Frontmatter</button>
         <div class="flex flex-row gap-2 w-full h-50">
             <div class="flex-1 shadow-4 overflow-scroll">
-                <Codemirror
-                    v-model="localModelValue"
-                    :indent-with-tab="true"
-                    :tab-size="4"
-                    :extensions="[minimalSetup, bracketMatching(), markdownLanguage, consoleLightExtension, lineNumbers(), highlightActiveLine(), highlightActiveLineGutter(), EditorState.allowMultipleSelections.of(true), drawSelection(), rectangularSelection(), crosshairCursor()]"
-                    :disabled="false"
-                    @update:modelValue="newValue => { localModelValue = newValue; $emit('update:modelValue', newValue); }"
-                    @ready="handleReady"
-                />
-            </div>
-            <div class="flex-1 shadow-4">
-                <iframe ref="markdownOutput" class="border-none" width="100%" height="100%" :src="iframeHtmlUrl" @load="() => { iframeLoaded = true; }">
-                </iframe>
+                <textarea>{{ basicExample }}</textarea>
             </div>
         </div>
     </section>
@@ -46,36 +28,9 @@
 
 <script setup>
 
-import Editor from '@toast-ui/editor';
-import '@toast-ui/editor/dist/toastui-editor.css';
-
 import { onMounted, ref, watch, watchEffect } from 'vue';
-import { Codemirror } from 'vue-codemirror';
-import { EditorView, lineNumbers, highlightActiveLine, highlightActiveLineGutter, drawSelection, rectangularSelection, crosshairCursor } from '@codemirror/view';
-import { EditorState } from '@codemirror/state';
-import { minimalSetup  } from 'codemirror';
-import { bracketMatching } from '@codemirror/language';
-import { markdownLanguage } from '@codemirror/lang-markdown';
-import { consoleLightExtension } from './codemirrorLightTheme.js';
-import markdownit from 'markdown-it';
 import DOMPurify from 'dompurify';
-
-// markdown-it plugins
-import { frontmatterPlugin } from '@mdit-vue/plugin-frontmatter';
-import markdownBracketedSpansPlugin from 'markdown-it-bracketed-spans';
-import markdownAttrsPlugin from 'markdown-it-attrs';
-import markdownContainerPlugin from 'markdown-it-container';
-import markdownKatexPlugin from '@vscode/markdown-it-katex';
-import markdownDeflistPlugin from 'markdown-it-deflist';
-import markdownFootnotePlugin from 'markdown-it-footnote';
-import markdownImplicitFiguresPlugin from 'markdown-it-implicit-figures';
-import markdownGridTablesPlugin from 'markdown-it-gridtables';
-import markdownSubPlugin from 'markdown-it-sub';
-import markdownSupPlugin from 'markdown-it-sup';
-import markdownTaskListsPlugin from 'markdown-it-task-lists';
-import markdownMarkPlugin from 'markdown-it-mark';
-import { full as markdownEmojiPlugin } from 'markdown-it-emoji';
-import markdownItSourceMap from 'markdown-it-source-map';
+import hypermd from 'hypermd';
 
 import basicExample from '../test/basic.md?raw';
 import bookCssRaw from '../test/book.css?raw';
@@ -83,179 +38,5 @@ import bookCssRaw from '../test/book.css?raw';
 import '/node_modules/primeflex/primeflex.css';
 import '/node_modules/primeflex/themes/primeone-light.css';
 import '/node_modules/github-markdown-css/github-markdown.css';
-
-import iframeHtmlUrl from './iframe.html?url';
-
-const emit = defineEmits(['update:modelValue']);
-const editor = ref();
-
-const outputChoice = ref('html');
-
-// DOM element references
-const markdownOutput = ref(null);
-const iframeLoaded = ref(false);
-
-async function renderMarkdown(source, format, element) {
-    if (!element) {
-        console.log('No element to render to');
-        return;
-    }
-    const iframe = element.contentDocument;
-    if (!iframe || !iframe.body) {
-        console.log('No iframe');
-        return;
-    }
-    // Generate sanitized HTML content from markdown source (also extracts frontmatter)
-    let env = {};
-    const startRenderTime = performance.now();
-    const output = DOMPurify.sanitize(md.render(source || '', env));
-    const endRenderTime = performance.now();
-    const totalRenderTime = endRenderTime - startRenderTime;
-    console.log(`Markdown HTML render took ${totalRenderTime}ms`);
-
-    if (format == 'frontmatter') {
-        element.contentWindow.postMessage({
-            action: 'update',
-            payload: {
-                html: `<pre>${JSON.stringify(env.frontmatter, null, 4)}</pre>`,
-            },
-        });
-    } else if (format == 'html') {
-        element.contentWindow.postMessage({
-            action: 'update',
-            payload: {
-                html: output,
-            },
-        });
-    } else if (format == 'paged') {
-        element.contentWindow.postMessage({
-            action: 'paged',
-            payload: {
-                html: output,
-                css: bookCssRaw,
-            },
-        });
-    }
-}
-
-const Theme = EditorView.theme({
-    "&": {
-        fontSize: "10.5pt",
-        border: "1px solid #c0c0c0"
-    },
-    ".cm-content": {
-        fontFamily: "Menlo, Monaco, Lucida Console, monospace",
-        minHeight: "200px"
-    },
-    ".cm-gutters": {
-        minHeight: "200px"
-    },
-    ".cm-scroller": {
-        overflow: "auto",
-        maxHeight: "600px"
-    }
-});
-
-const containerNames = [ 'spoiler', 'warning' ];
-
-function multiuseContainers(names, md) {
-    for (const name of names) {
-        md = md.use(markdownContainerPlugin, name);
-    }
-    return md;
-}
-
-const md = multiuseContainers(containerNames, markdownit({
-    html: true,
-    breaks: false,
-    linkify: true,
-    quotes: '“”‘’',
-})
-.use(frontmatterPlugin, {})
-.use(markdownAttrsPlugin, {})
-.use(markdownBracketedSpansPlugin, {})
-.use(markdownKatexPlugin)
-.use(markdownDeflistPlugin)
-.use(markdownFootnotePlugin)
-.use(markdownImplicitFiguresPlugin, {
-    figcaption: true,
-    keepAlt: true,
-})
-.use(markdownGridTablesPlugin)
-.use(markdownSubPlugin)
-.use(markdownSupPlugin)
-.use(markdownTaskListsPlugin)
-.use(markdownMarkPlugin)
-.use(markdownEmojiPlugin)
-.use(markdownItSourceMap)
-);
-
-//
-// Inject line numbers for sync scroll. Notes:
-//
-// - We track only headings and paragraphs on first level. That's enough.
-// - Footnotes content causes jumps. Level limit filter it automatically.
-function injectLineNumbers (tokens, idx, options, env, slf) {
-    let line;
-    if (tokens[idx].map && tokens[idx].level === 0) {
-        line = tokens[idx].map[0];
-        tokens[idx].attrJoin('class', 'line');
-        tokens[idx].attrSet('data-line', String(line));
-    }
-    return slf.renderToken(tokens, idx, options, env, slf);
-}
-
-//md.renderer.rules.paragraph_open = md.renderer.rules.heading_open = injectLineNumbers;
-//md.renderer.rules.footnote_open = injectLineNumbers;
-console.log(md.renderer.rules);
-
-const editorObject = ref();
-
-let localModelValue = ref(basicExample);
-
-function updateView() {
-    renderMarkdown(localModelValue.value, outputChoice.value, markdownOutput.value);
-}
-
-watchEffect(() => {
-    updateView();
-});
-
-watch(iframeLoaded, () => {
-    updateView();
-});
-
-function handleReady(payload) {
-    editorObject.value = payload;
-}
-
-// Step 1: Define the user plugin function
-function latexPlugin() {
-    const toHTMLRenderers = {
-        sup(node) {
-            const body = 'content';
-
-            return [
-                { type: 'openTag', tagName: 'sup', outerNewLine: false },
-                { type: 'html', content: body },
-                { type: 'closeTag', tagName: 'sup', outerNewLine: false }
-            ];
-        },
-    };
-    return { toHTMLRenderers };
-}
-
-onMounted(() => {
-    const e = new Editor({
-        el: editor.value,
-        height: '500px',
-        initialEditType: 'markdown',
-        previewStyle: 'vertical',
-        plugins: [latexPlugin],
-        events: {
-            change: () => emit('update:modelValue', e.getMarkdown()),
-        },
-    });
-});
 
 </script>
--- app/test/basic.md
+++ app/test/basic.md
@@ -1,19 +1,3 @@
----
-# This is the frontmatter inside the --- fenced block.
-# Format is YAML keyword style. Keywords indicate document
-# metadata for PDF and EPUB generation.
-title: Test Document
-subtitle: Demonstration of many features of Nathan's Book Tool
-date: 2024-08-19
-authors:
-    - name: Nathan Whitehead
-      affiliations: 
-        - Shimmer Math Labs
-pagesize: 6x9
-theme: basic
-typeface: 100%
----
-
 # Main Title
 
 Some text is here. You can also put [links](http://www.example.com/) to URLs and things.
