0908f6ee4674e482fc355ef17258c3be452bf96d
Nathan Whitehead <nwhitehe@gmail.com>
2024-10-02 14:43:03 -0600

    Use vue in electron demo.

---
+++ app/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your flashy Electron application.</p>
+</template>
+  
+<script setup>
+import './index.css';
+
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, session, BrowserWindow } from 'electron';
 import path from 'path';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -27,7 +27,17 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+    createWindow();
+    // session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    //     callback({
+    //         responseHeaders: {
+    //             ...details.responseHeaders,
+    //             'Content-Security-Policy': ['default-src \'self\'']
+    //         }
+    //     });
+    // });    
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
