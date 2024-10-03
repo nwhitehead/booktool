6af2b409de1434abc1bffa104b57da1bdfb7585b
Nathan Whitehead <nwhitehe@gmail.com>
2024-10-03 11:01:22 -0600

    Import index from old app.

--- app/src/renderer.ts
+++ app/src/renderer.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import VueCodemirror from 'vue-codemirror';
+
+const app = createApp(App);
+app.use(VueCodemirror, {
+    // Set global Codemirror extension default list to empty
+    // Otherwise we always get basic-setup extensions and cannot turn them off in components
+    extensions: []
+});
+app.mount('#app');
