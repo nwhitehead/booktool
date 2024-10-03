89c884cf97563131bbd8890c6c6c8e2684a94d79
Nathan Whitehead <nwhitehe@gmail.com>
2024-10-03 09:54:16 -0600

    Demo with action.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,16 @@
 <template>
     <h1>💖 Hello World!</h1>
     <p>Welcome to your flashy Electron application.</p>
+    <button @click="number += 1;">Increment</button>
+    <p>Clicked {{  number }} times</p>
 </template>
   
 <script setup>
+
+import { ref } from 'vue';
+
 import './index.css';
 
-console.log('👋 This message is being logged by "App.vue", included via Vite');
+let number = ref(0);
+
 </script>
