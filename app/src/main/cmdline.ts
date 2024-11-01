ac58e334e02d64cb44599ce8089f35e6c2ad961b
Nathan Whitehead <nwhitehe@gmail.com>
2024-11-01 09:24:08 -0600

    Add better option parsing and usage printing.

--- app/src/main/cmdline.ts
+++ app/src/main/cmdline.ts
@@ -1,27 +1,66 @@
 import commandLineArguments from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
 import process from 'node:process';
 import { handleRender } from './render.ts';
 
-const introMessage = `
-BookTool
-========
-
-Convert Markdown into publications.
-
-`;
-
 // Command-line arguments
 const commandLineOptions = [
-    { name: 'help', alias: 'h', type: Boolean },
-    { name: 'root', alias: 'r', type: String, defaultOption: '.' },
+    {
+        name: 'help',
+        alias: 'h',
+        type: Boolean,
+        description: 'Show this usage guide.',
+    },
+    {
+        name: 'root',
+        alias: 'r',
+        type: String,
+        typeLabel: '{underline path}',
+        defaultValue: '.',
+        description: 'Set root directory for document generation.',
+    },
+    {
+        name: 'index',
+        alias: 'i',
+        type: String,
+        typeLabel: '{underline filename}',
+        lazyMultiple: true,
+        defaultValue: ['index.md'],
+        description: 'Set starting index filename. Can be set multiple times. The first file that exists will be used.',
+    },
 ];
+const logo = `
+██████╗  ██████╗  ██████╗ ██╗  ██╗████████╗ ██████╗  ██████╗ ██╗     
+██╔══██╗██╔═══██╗██╔═══██╗██║ ██╔╝╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+██████╔╝██║   ██║██║   ██║█████╔╝    ██║   ██║   ██║██║   ██║██║     
+██╔══██╗██║   ██║██║   ██║██╔═██╗    ██║   ██║   ██║██║   ██║██║     
+██████╔╝╚██████╔╝╚██████╔╝██║  ██╗   ██║   ╚██████╔╝╚██████╔╝███████╗
+╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
+`;
+const sections = [
+    {
+        content: logo,
+        raw: true,
+    },
+    {
+        header: 'BookTool',
+        content: 'Convert Markdown into publications. Generate PDF, ePub, and HTM.',
+    },
+    {
+        header: 'Main Options',
+        optionList: commandLineOptions,
+    }
+];
+
+const usage = commandLineUsage(sections);
 
 function main(options) {
     if (options.help) {
-
+        console.log(usage);
+        return;
     }
-    console.log(introMessage);
     console.log(options);
+    console.log(options.root);
 }
 
 // Main invocation
