5f512bf3356c8d468c43fa870cccc6e0a6ac0488
Nathan Whitehead <nwhitehe@gmail.com>
2024-11-16 16:58:26 -0700

    Call tex algo after pagination, with right options for justify and last line justify.

--- app/test/main.ts
+++ app/test/main.ts
@@ -1,52 +1,10 @@
 
+
 import { hyphenate } from 'hyphen/en';
 import { texLinebreakDOM } from 'tex-linebreak2';
-import { Previewer, Handler } from './pagedjs/src/index.js';
+import { Previewer } from './pagedjs/src/index.js';
 
 let paged = new Previewer();
-
-document.getElementById('buttonRegister').onclick = function() {
-
-    class MyHandler extends Handler {
-        constructor(chunker, polisher, caller) {
-            super(chunker, polisher, caller);
-        }
-
-        onPageLayout(wrapper, token, layout) {
-            console.log(wrapper, token, layout, layout.bounds.width, token.node);
-            let elems;
-            if (token.node.querySelectorAll) {
-                elems = token.node.querySelectorAll('p');
-                console.log(elems);
-                texLinebreakDOM(elems, {
-                    justify: true,
-                    stripSoftHyphensFromOutputText: false,
-                    updateOnWindowResize: false,
-                    lineWidth: 100,
-                });    
-            }
-    }
-        // beforePageLayout(page) {
-        // }
-        // layoutNode(node, layout) {
-        //     const width = layout.bounds.width;
-        //     if (node.nodeName === 'P') {
-        //         console.log('layoutNode', node, layout.bounds.width, layout);
-        //         // const gcs = getComputedStyle(node);
-        //         const gcs = getComputedStyle(layout.element);
-        //         let { width, boxSizing, paddingLeft, paddingRight, textIndent, lineHeight } = getComputedStyle(node);
-        //         // console.log('layoutNode', gcs);
-        //         texLinebreakDOM(node, {
-        //             justify: true,
-        //             stripSoftHyphensFromOutputText: false,
-        //             updateOnWindowResize: false,
-        //             lineWidth: 200,
-        //         });
-        //     }
-        // }
-    }
-    paged.registerHandlers(MyHandler);
-}
 
 document.getElementById('buttonAddHyphens').onclick = async function() {
     const elems = document.querySelectorAll('p');
@@ -55,6 +13,15 @@ document.getElementById('buttonAddHyphens').onclick = async function() {
     }
 }
 
-document.getElementById('buttonPaginate').onclick = function() {
-    paged.preview();
+document.getElementById('buttonPaginate').onclick = async function() {
+    await paged.preview();
+    for (const para of document.querySelectorAll('p')) {
+        const style = getComputedStyle(para);
+        console.log(style);
+        texLinebreakDOM(para, {
+            justify: style['text-align'] === 'justify',
+            updateOnWindowResize: false,
+            addInfiniteGlueToFinalLine: !(style['text-align-last'] === 'justify'),
+        });
+    }
 }
